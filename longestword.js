42d8ef9d50a3d37dd2f674c8eaf91eb519baad5a
christoulakisa <xristoulakis@gmail.com>
2016-02-10 12:45:51 +0200

    Update longestword.js

--- longestword.js
+++ longestword.js
@@ -2,28 +2,12 @@
 
 function findLongestWord(str) {
   var counter=0, max=0;
-/*string to array*/
-  var a=str.split("");
+  var a=str.split(" ");
   for(var i=0;i<=a.length-1;i++){
-      if (a[i]===" " ){
-          if( counter>max){
-              max=counter;
-                }
-          counter=0;
+      if(a[i].length > max){
+          max=a[i].length;
       }
-      /*last character is not a space*/
-      else if (i===a.length-1){
-          counter++;
-          if( counter>max){
-              max=counter;
-                }
-      }
-      /*non-space character*/
-       else if(a[i]!==" "){
-          counter++;
-        } 
   }
   console.log(max);
   return max;
 }
-findLongestWord("What if we try a super-long word such as otorhinolaryngology");
